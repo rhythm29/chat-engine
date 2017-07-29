5f0445fe774a103bac588769aba43706e88b31e9
Rhythm <rhythmwalia29@gmail.com>
2017-07-29 18:18:33 +0200

    Basic chat service

--- client.js
+++ client.js
@@ -1,17 +1,42 @@
 /*
-Sends a msg to another client B, via char server.
+Sends a msg to another client B, via char server. Client also listens
+on a port specified by commandline so that the server can push the
+messages from other client
 */
+const express = require('express')
+const clientApp = express()
+const bodyParser = require('body-parser');
 var request=require('request');
 
+
+clientApp.use(bodyParser.json());
+var args = process.argv.slice(2);
+console.log(args)
+var client1 = args[0];
+var client2 = args[1]
+var clientPort = parseInt(args[2]);
+
+clientApp.post('/', function (req, res){
+  console.log(req.body.msg);
+  res.send('{"msg":"ok"}')    
+});
+
+
+clientApp.listen(clientPort, function () {
+  console.log('I am the Chat client running at ' + clientPort)
+})
+
+//Client is sending request here
+
 var msg = {
-  "sender": "A",
-  "receiver": "B",
+  "sender": client1,
+  "receiver": client2,
   "type": "push",
   "msg": "Ich Leibe Berlin"
 };
 
 var options = {
-  url: 'http://localhost:3000',
+  url: 'http://localhost:4000',
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
--- server.js
+++ server.js
@@ -1,15 +1,48 @@
 /*
-Receives a message from client
+Receives a message from client and then forward it to receiver
+mentioned in the message 
 */
 
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
 app.post('/', function (req, res) {
-	console.log(req.body)
+	console.log("hello")
+  	sendChatFurther(req.body)	
+  	res.send('{"msg":"ok"}')
+  
 });
 
-app.listen(3000, function () {
-  console.log('I am the server 3000!')
+app.listen(4000, function () {
+  console.log('I am the server 4000!')
 });
+
+function sendChatFurther(body){
+	sender = body['sender'];
+	receiver = body['receiver'];
+	switch (receiver){
+		case "A":
+		 var port = 8000
+		 break;
+		case "B":
+		 port = 9000
+		 break;
+	}
+	var options = {
+    url: "http://localhost:"+port,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    json: body
+  };
+  var request=require('request');
+  	request(options, function(err, res, body) {
+    	if (res && (res.statusCode === 200 || res.statusCode === 201)) {
+      	console.log("message sent to"+ receiver);
+      }
+    });
+}
